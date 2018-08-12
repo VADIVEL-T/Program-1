c8b5de2cb94365713488c61684413333078a1fbb
VADIVEL T <42204006+VADIVEL-T@users.noreply.github.com>
2018-08-12 11:21:52 +0530

    Add files via upload

---
+++ Largest Of Three.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,k,a,b,c;
+	printf("Enter A number");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a>b)
+	{
+		printf("%d",a);
+	}
+	else if(b>c)
+	{
+		printf("%d",b);
+	}
+	else if(c>a)
+	{
+		printf("%d",c);
+	}
+	
+}
